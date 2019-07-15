da2101b1472cc37d4547faaf6a4e97564ac4ca99
Alexander Mayo <mayoalexandertd@icloud.com>
2019-07-15 17:00:23 -0500

    created the baseline test

--- test/index.js
+++ test/index.js
@@ -1,16 +1,20 @@
+const axios = require('axios')
+const expect = require('chai').expect
+
 describe('API Initialization', async () => {
 
-  it('Should start the test suite', () => {
-    axios.get('/your/endpoint') {
-      results = res
-    }
-
-
-    expect(results).to.equal({
-
+  it('API should return status of 200', async () => {
+    await axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+      expect(res.status).to.equal(200)
     })
   })
 
-  it('Needs to fetch and return CSRF Token')
+  it('API should return a title and a status', async () => {
+    await axios.get('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
+      expect(res.data.title).to.be.ok
+      expect(res.data.userId).to.be.ok
+      expect(res.data.id).to.be.ok
+    })
+  })
 
 })
