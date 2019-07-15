4f05464163db167f2d655aabaeec17f2ea505713
Alexander Mayo <mayoalexandertd@icloud.com>
2019-07-15 16:37:44 -0500

    initial commit

---
+++ test/index.js
@@ -0,0 +1,16 @@
+describe('API Initialization', async () => {
+
+  it('Should start the test suite', () => {
+    axios.get('/your/endpoint') {
+      results = res
+    }
+
+
+    expect(results).to.equal({
+
+    })
+  })
+
+  it('Needs to fetch and return CSRF Token')
+
+})
